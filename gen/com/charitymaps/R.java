e2b2a1ac7d8ca4e1028f4f0ee68547fe14be295d
dazine <daan_grootenboer@hotmail.com>
2013-06-13 15:30:01 +0200

    database aanpassen
    bug aangepast

--- gen/com/charitymaps/R.java
+++ gen/com/charitymaps/R.java
@@ -193,38 +193,36 @@ containing a value of this type.
     }
     public static final class id {
         public static final int btnCreateProduct=0x7f050013;
-        public static final int btnDelete=0x7f050015;
-        public static final int btnSave=0x7f050014;
-        public static final int btnViewProducts=0x7f050018;
+        public static final int btnViewProducts=0x7f050016;
         public static final int button_database=0x7f05000a;
         public static final int button_enter=0x7f05000e;
         public static final int button_maps=0x7f050007;
         public static final int button_overzicht=0x7f05000b;
         public static final int hybrid=0x7f050004;
-        public static final int hybrid_map=0x7f050020;
+        public static final int hybrid_map=0x7f05001e;
         public static final int imageView1=0x7f050005;
         public static final int inputDesc=0x7f050012;
         public static final int inputName=0x7f050010;
         public static final int inputPrice=0x7f050011;
         public static final int map=0x7f05000f;
-        public static final int map_types=0x7f05001c;
-        public static final int name=0x7f050017;
+        public static final int map_types=0x7f05001a;
+        public static final int name=0x7f050015;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
-        public static final int normal_map=0x7f05001d;
-        public static final int pid=0x7f050016;
+        public static final int normal_map=0x7f05001b;
+        public static final int pid=0x7f050014;
         public static final int satellite=0x7f050002;
-        public static final int satellite_map=0x7f05001e;
+        public static final int satellite_map=0x7f05001c;
         public static final int terrain=0x7f050003;
-        public static final int terrain_map=0x7f05001f;
+        public static final int terrain_map=0x7f05001d;
         public static final int textView1=0x7f050009;
         public static final int textView2=0x7f050008;
         public static final int textView3=0x7f05000c;
         public static final int textView4=0x7f05000d;
         public static final int textView5=0x7f050006;
-        public static final int txtDate=0x7f05001a;
-        public static final int txtDescription=0x7f05001b;
-        public static final int txtTitle=0x7f050019;
+        public static final int txtDate=0x7f050018;
+        public static final int txtDescription=0x7f050019;
+        public static final int txtTitle=0x7f050017;
     }
     public static final class layout {
         public static final int activity_intro=0x7f030000;
@@ -253,6 +251,7 @@ containing a value of this type.
         /**   Requested by string saying which app requested the notification. [CHAR LIMIT=42] 
          */
         public static final int auth_client_requested_by_msg=0x7f060011;
+        public static final int beschrijving=0x7f06002e;
         /**  Button in confirmation dialog to enable Google Play services.  Clicking it
         will direct user to application settings of Google Play services where they
         can enable it [CHAR LIMIT=40] 
@@ -322,6 +321,7 @@ containing a value of this type.
         public static final int intro_maps=0x7f060026;
         public static final int intro_overzicht=0x7f060028;
         public static final int intro_welkom=0x7f060024;
+        public static final int locatie=0x7f06002d;
         /**  Location client code resources (prefix with location_client) 
 
         Begin strings for location.reporting
@@ -340,6 +340,7 @@ containing a value of this type.
         public static final int satellite_map=0x7f06001a;
         public static final int select_map=0x7f060018;
         public static final int terrain_map=0x7f06001b;
+        public static final int titel=0x7f06002c;
     }
     public static final class style {
         /** 
--- src/com/charitymaps/EditProductActivity.java
+++ src/com/charitymaps/EditProductActivity.java
@@ -16,17 +16,17 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditProductActivity extends Activity {
 
-	EditText txtName;
-	EditText txtPrice;
-	EditText txtDesc;
-	EditText txtCreatedAt;
+	TextView txtName;
+	TextView txtPrice;
+	TextView txtDesc;
+	TextView txtCreatedAt;
 	Button btnSave;
 	Button btnDelete;
 
@@ -41,12 +41,6 @@ public class EditProductActivity extends Activity {
 	// single product url
 	private static final String url_product_detials = "http://www.gowithus.nl/projects/android/get_product_details.php";
 
-	// url to update product
-	private static final String url_update_product = "http://www.gowithus.nl/projects/android/update_product.php";
-	
-	// url to delete product
-	private static final String url_delete_product = "http://www.gowithus.nl/projects/android/delete_product.php";
-
 	// JSON Node names
 	private static final String TAG_SUCCESS = "success";
 	private static final String TAG_PRODUCT = "product";
@@ -60,10 +54,12 @@ public class EditProductActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_product);
 
-		// save button
-		btnSave = (Button) findViewById(R.id.btnSave);
-		btnDelete = (Button) findViewById(R.id.btnDelete);
-
+		// BELANGRIJK!!
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+		      StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		      StrictMode.setThreadPolicy(policy);
+		}
+		
 		// getting product details from intent
 		Intent i = getIntent();
 		
@@ -72,26 +68,6 @@ public class EditProductActivity extends Activity {
 
 		// Getting complete product details in background thread
 		new GetProductDetails().execute();
-
-		// save button click event
-		btnSave.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// starting background task to update product
-				new SaveProductDetails().execute();
-			}
-		});
-
-		// Delete button click event
-		btnDelete.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// deleting product in background thread
-				new DeleteProduct().execute();
-			}
-		});
 
 	}
 
@@ -107,7 +83,7 @@ public class EditProductActivity extends Activity {
 		protected void onPreExecute() {
 			super.onPreExecute();
 			pDialog = new ProgressDialog(EditProductActivity.this);
-			pDialog.setMessage("Loading details, please wait...");
+			pDialog.setMessage("Loading, please wait...");
 			pDialog.setIndeterminate(false);
 			pDialog.setCancelable(true);
 			pDialog.show();
@@ -148,9 +124,9 @@ public class EditProductActivity extends Activity {
 
 							// product with this pid found
 							// Edit Text
-							txtName = (EditText) findViewById(R.id.inputName);
-							txtPrice = (EditText) findViewById(R.id.inputPrice);
-							txtDesc = (EditText) findViewById(R.id.inputDesc);
+							txtName = (TextView) findViewById(R.id.inputName);
+							txtPrice = (TextView) findViewById(R.id.inputPrice);
+							txtDesc = (TextView) findViewById(R.id.inputDesc);
 
 							// display product data in EditText
 							txtName.setText(product.getString(TAG_NAME));
@@ -176,140 +152,6 @@ public class EditProductActivity extends Activity {
 		protected void onPostExecute(String file_url) {
 			// dismiss the dialog once got all details
 			pDialog.dismiss();
-		}
-	}
-
-	/**
-	 * Background Async Task to  Save product Details
-	 * */
-	class SaveProductDetails extends AsyncTask<String, String, String> {
-
-		/**
-		 * Before starting background thread Show Progress Dialog
-		 * */
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			pDialog = new ProgressDialog(EditProductActivity.this);
-			pDialog.setMessage("Saving...");
-			pDialog.setIndeterminate(false);
-			pDialog.setCancelable(true);
-			pDialog.show();
-		}
-
-		/**
-		 * Saving product
-		 * */
-		protected String doInBackground(String... args) {
-
-			// getting updated data from EditTexts
-			String name = txtName.getText().toString();
-			String price = txtPrice.getText().toString();
-			String description = txtDesc.getText().toString();
-
-			// Building Parameters
-			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			params.add(new BasicNameValuePair(TAG_PID, pid));
-			params.add(new BasicNameValuePair(TAG_NAME, name));
-			params.add(new BasicNameValuePair(TAG_PRICE, price));
-			params.add(new BasicNameValuePair(TAG_DESCRIPTION, description));
-
-			// sending modified data through http request
-			// Notice that update product url accepts POST method
-			JSONObject json = jsonParser.makeHttpRequest(url_update_product,
-					"POST", params);
-
-			// check json success tag
-			try {
-				int success = json.getInt(TAG_SUCCESS);
-				
-				if (success == 1) {
-					// successfully updated
-					Intent i = getIntent();
-					// send result code 100 to notify about product update
-					setResult(100, i);
-					finish();
-				} else {
-					// failed to update product
-				}
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-
-			return null;
-		}
-
-
-		/**
-		 * After completing background task Dismiss the progress dialog
-		 * **/
-		protected void onPostExecute(String file_url) {
-			// dismiss the dialog once product uupdated
-			pDialog.dismiss();
-		}
-	}
-
-	/*****************************************************************
-	 * Background Async Task to Delete Product
-	 * */
-	class DeleteProduct extends AsyncTask<String, String, String> {
-
-		/**
-		 * Before starting background thread Show Progress Dialog
-		 * */
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			pDialog = new ProgressDialog(EditProductActivity.this);
-			pDialog.setMessage("Deleting...");
-			pDialog.setIndeterminate(false);
-			pDialog.setCancelable(true);
-			pDialog.show();
-		}
-
-		/**
-		 * Deleting product
-		 * */
-		protected String doInBackground(String... args) {
-
-			// Check for success tag
-			int success;
-			try {
-				// Building Parameters
-				List<NameValuePair> params = new ArrayList<NameValuePair>();
-				params.add(new BasicNameValuePair("pid", pid));
-
-				// getting product details by making HTTP request
-				JSONObject json = jsonParser.makeHttpRequest(
-						url_delete_product, "POST", params);
-
-				// check your log for json response
-				Log.d("Delete Product", json.toString());
-				
-				// json success tag
-				success = json.getInt(TAG_SUCCESS);
-				if (success == 1) {
-					// product successfully deleted
-					// notify previous activity by sending code 100
-					Intent i = getIntent();
-					// send result code 100 to notify about product deletion
-					setResult(100, i);
-					finish();
-				}
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-
-			return null;
-		}
-
-		/**
-		 * After completing background task Dismiss the progress dialog
-		 * **/
-		protected void onPostExecute(String file_url) {
-			// dismiss the dialog once product deleted
-			pDialog.dismiss();
-
 		}
 
 	}
