2bdaaebf7a90eb488bd75f446385581a6fc9321e
Jermain <0867046@hr.nl>
2013-06-17 13:24:33 +0200

    Engels Ondersteund
    strings met engelse taal toegevoegd

--- src/com/charitymaps/IntroActivity.java
+++ src/com/charitymaps/IntroActivity.java
@@ -1,14 +1,50 @@
 package com.charitymaps;
 
 import android.os.Bundle;
-import android.app.Activity;
+//import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-public class IntroActivity extends Activity implements View.OnClickListener {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.widget.ShareActionProvider;
+
+public class IntroActivity extends SherlockActivity implements View.OnClickListener {
+
+	private ShareActionProvider mShareActionProvider;
 
 	Button button_maps;
+	Button button_database;
+	Button button_overzicht;
+	
+	 @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        /** Inflating the current activity's menu with res/menu/items.xml */
+	        getSupportMenuInflater().inflate(R.menu.items, menu);
+	 
+	        /** Getting the actionprovider associated with the menu item whose id is share */
+	        mShareActionProvider = (ShareActionProvider) menu.findItem(R.id.share).getActionProvider();
+	 
+	        /** Getting the target intent */
+	        Intent intent = getDefaultShareIntent();
+	 
+	        /** Setting a share intent */
+	        if(intent!=null)
+	            mShareActionProvider.setShareIntent(intent);
+	        return super.onCreateOptionsMenu(menu);
+	 
+	    }
+	 
+	    /** Returns a share intent */
+	    private Intent getDefaultShareIntent(){
+	 
+	        Intent intent = new Intent(Intent.ACTION_SEND);
+	        intent.setType("text/plain");
+	        intent.putExtra(Intent.EXTRA_SUBJECT, "SUBJECT");
+	        intent.putExtra(Intent.EXTRA_TEXT,"Sample Content !!!");
+	        return intent;
+	    }
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +52,12 @@ public class IntroActivity extends Activity implements View.OnClickListener {
 		setContentView(R.layout.activity_intro);
 		button_maps = (Button) findViewById(R.id.button_maps);
         button_maps.setOnClickListener(this);
+        
+        button_database = (Button) findViewById(R.id.button_database);
+        button_database.setOnClickListener(this);
+        
+        button_overzicht = (Button) findViewById(R.id.button_overzicht);
+        button_overzicht.setOnClickListener(this);
 	}
 
 	@Override
@@ -24,6 +66,14 @@ public class IntroActivity extends Activity implements View.OnClickListener {
     	case R.id.button_maps:
     		startActivity(new Intent("com.charitymaps.MainActivity"));
     		break;
+    	case R.id.button_database:
+    		startActivity(new Intent(this, AllProductsActivity.class));
+    		break;
+    	case R.id.button_overzicht:
+    		startActivity(new Intent(this, Overzicht.class));
+    		break;
     	}
+    	
     }
+	
 }
